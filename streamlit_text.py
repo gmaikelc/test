6e3f78e3d2923fd04f99929ad3a7e65b68d6ff92
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-06-04 14:37:02 -0500

    Update streamlit_text.py

--- streamlit_text.py
+++ streamlit_text.py
@@ -8,21 +8,19 @@ import streamlit as st
 #streamlit run C:\Users\gmaikelc\anaconda3\Lib\site-packages\ipykernel_launcher.py
 
 # Input for 'Degree of polymerization'
-#degree_of_polymerization = st.number_input('Degree of polymerization', min_value=0)
+degree_of_polymerization = st.number_input('Degree of polymerization', min_value=0)
 
 
 # Create a layout with 3 columns
-col1, col2, col3 = st.beta_columns(3)
+#col1, col2, col3 = st.beta_columns(3)
 
-# Use the middle column for the input box
-degree_of_polymerization = col1.number_input('Degree of polymerization', min_value=0)
 
 # Input for 'Percent'
-percent = col2.number_input('Percent', min_value=0, max_value=100)
+percent = st.number_input('Percent', min_value=0, max_value=100)
 
 # Dropdown for 'PM' and 'DP'
 options = ['PM', 'DP']
-choice = col3.selectbox('Choose an option', options)
+choice = st.selectbox('Choose an option', options)
 
 st.write(f"You entered {degree_of_polymerization} for Degree of polymerization, {percent}% for Percent, and selected {choice} from the dropdown.")
 
