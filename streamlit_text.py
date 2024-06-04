1e9e938a539dbf1a4d2c7d92344a975071f90c2b
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-06-04 15:10:54 -0500

    Update streamlit_text.py

--- streamlit_text.py
+++ streamlit_text.py
@@ -22,4 +22,51 @@ choice = col3.selectbox('Choose an option', options)
 
 st.write(f"You entered {degree_of_polymerization} for Degree of polymerization, {percent}% for Percent, and selected {choice} from the dropdown.")
 
+# Define the strings
+DM = '*[Si](C)(C)OI'
+DP = '*[Si](c1ccccc1)(c1ccccc1)OI'
+PM = '*[Si](c1ccccc1)(C)OI'
+left_end = 'C[Si](C)(C)O*'
+right_end = '*[Si](C)(C)C'
+additional_string = 'ICCC*'
 
+if st.button('Press to generate the silicon oil structure based on the parameters'):
+  # Calculate Number_Rep_Unit_2
+  Number_Rep_Unit_2 = round(percent / 100 * degree_of_polymerization)
+  print('Number of repeating units:',Number_Rep_Unit_2)
+
+  # Perform the division and get the integer quotient and remainder
+  Number_Rep_Unit_1 = degree_of_polymerization - Number_Rep_Unit_2 -2   # Assuming a value for DiMethyl for the code to run
+  ratio_rep_unit, remainder = divmod(Number_Rep_Unit_1, Number_Rep_Unit_2 + 1)
+
+  # Print the results
+  print("Ratio (Integer part):", ratio_rep_unit)
+  print("Remainder:", remainder)
+
+  # Number of iterations
+  num_it = Number_Rep_Unit_2
+  print('Number of iterations:', num_it)
+
+  
+  # Perform the concatenation for the specified number of iterations
+ 
+  # Initialize the final string with the left end
+  end_ru = DM*ratio_rep_unit
+  f_ru =''
+  for i in range(num_it):
+      # Add the DM string ratio_DM times
+      f_ru += DM * ratio_DM
+      if choice='DP':
+        f_ru+=DP
+      elif choice='PM':
+        f_ru+=PM
+
+  n_ru=f_ru+end_ru
+
+  si_oil=left_end+n_ru+right_end
+  
+  # Add the right end to the final string
+  #final_string += right_end
+
+  # Print the final string
+  print(si_oil)
