d31ebcfa4e1a2a6858f09c0f641e894389703d40
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-06-04 14:34:18 -0500

    Update streamlit_text.py

--- streamlit_text.py
+++ streamlit_text.py
@@ -15,14 +15,14 @@ import streamlit as st
 col1, col2, col3 = st.beta_columns(3)
 
 # Use the middle column for the input box
-degree_of_polymerization = col2.number_input('Degree of polymerization', min_value=0)
+degree_of_polymerization = col1.number_input('Degree of polymerization', min_value=0)
 
 # Input for 'Percent'
-percent = st.number_input('Percent', min_value=0, max_value=100)
+percent = col2.number_input('Percent', min_value=0, max_value=100)
 
 # Dropdown for 'PM' and 'DP'
 options = ['PM', 'DP']
-choice = st.selectbox('Choose an option', options)
+choice = col3.selectbox('Choose an option', options)
 
 st.write(f"You entered {degree_of_polymerization} for Degree of polymerization, {percent}% for Percent, and selected {choice} from the dropdown.")
 
