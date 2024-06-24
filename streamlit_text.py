b08c4765012d7ad8d02c2517b191ca86ad6b8a71
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-06-24 12:51:41 -0500

    Update streamlit_text.py

--- streamlit_text.py
+++ streamlit_text.py
@@ -113,7 +113,7 @@ def generate_si_oil_pattern(choice, percent, degree_of_polymerization, num_1=1, 
 
     return si_oil_pattern
 
-def generate_si_oil(choice, percent, degree_of_polymerization):
+def generate_si_oil(choice, percent, degree_of_polymerization,DM,DP,PM):
     # Calculate Number_Rep_Unit_2
     st.write('percent:',percent)
     Number_Rep_Unit_2 = round(percent/100 * degree_of_polymerization)
